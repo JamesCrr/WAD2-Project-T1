5a1820e05d8e5a5b6b8f8636f61d9e3a17ad605d
chingupingu <p.shuen01@gmail.com>
2023-10-26 16:39:46 +0800

    modal for donation to event
    added model after donate button is clicked on event page
    
    also made the success and error pages after payment look nicer

--- frontend/src/views/ErrorPage.vue
+++ frontend/src/views/ErrorPage.vue
@@ -1,3 +1,31 @@
 <template>
-    <h1>payment failed</h1>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <div class="card error-card">
+        <!-- error svg -->
+        <i class="bi bi-x-octagon"></i>
+        <!-- card body -->
+        <div class="card-body">
+            <h5 class="card-title">Payment failed :(</h5>
+            <p class="card-text">Something went wrong.</p>
+            <button @click="$router.go(-3)" class="btn btn-primary">Return to Event Page</button>
+        </div>
+    </div>
 </template>
+
+
+<style>
+.error-card {
+    position: relative;
+    margin-left: auto;
+    margin-right: auto;
+    top: 10rem;
+    width: 25rem;
+}
+.bi-x-octagon {
+    color: red;
+    font-size: 8rem;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 1rem;
+}
+</style>
