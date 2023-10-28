a52cbed8051592f7f6a17471d2491774d6e81c08
gohgrace02 <gohgrace02@gmail.com>
2023-10-28 23:42:31 +0800

    Updated HomeView

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,18 +4,60 @@
 
   <!-- content -->
   <div class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <router-link v-for="event of events" v-bind:to="'/events/' + event.id" :key="event.id">
-            {{ event.id }}
-          </router-link>
+    <div class="container-fluid">
+      <div class="row">
+        <router-link v-for="event of events" v-bind:to="'/events/' + event.id" :key="event.id">
+          {{ event.id }}
+        </router-link>
+      </div>
+    
+      <div class="row">
+        <div class="video-wrap">
+          <video autoplay="" loop="" muted="" class="custom-video" poster="">
+              <source src="../assets/WADII.mp4" type="video/mp4">
+              Your browser does not support the video tag.
+          </video>
         </div>
-    </div>
-    <div class="dashboard-content p-3">
-        <h2 class="fs-5"> Dashboard</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, totam? Sequi alias eveniet ut quas
-            ullam delectus et quasi incidunt rem deserunt asperiores reiciendis assumenda doloremque provident,
-            dolores aspernatur neque.</p>
+        <img src="https://static.wixstatic.com/media/3ecde9_a13d7ce172b6448396516afcfb7a2fa0~mv2.gif" alt="" style="width: 100%; height: 54px; object-fit: cover;" width="980" height="54" fetchpriority="high">
+      </div>
+      
+      <div class="row" style="background-color: white; margin: 0;">
+        <div class="img-div col-lg-5 col-md-5 col-sm-12">
+          <img class="img-info" src="../assets/home/about-us.jpg" alt="">
+        </div>
+        <div class="col-lg-7 col-md-7 col-sm-12 p-5">
+          <h2> About us</h2>
+          <p>At EcoConnect, we are driven by a shared passion for our planet and a collective belief in the power of
+            environmental action. In the face of pressing environmental challenges, we bridge the gap between local
+            green initiatives and the individuals eager to make a difference.</p>
+        </div>
+      </div>
+
+      <div class="row" style="background-color: pink; margin: 0;">
+        <div class="col-lg-7 col-md-7 col-sm-12 p-5 order-lg-1 order-md-1 order-sm-2">
+          <h2>Our mission</h2>
+          <p>
+            Our goal is to empower local organizers and eco-conscious individuals. We provide a platform to boost their impact, 
+            reach a wider audience, and create a strong community. At the same time, we connect green-minded people with meaningful 
+            opportunities to volunteer, contribute, and embrace eco-friendly actions. Join us for a greener world.
+          </p>
+        </div>
+        <div class="img-div col-lg-5 col-md-5 col-sm-12 order-lg-2 order-md-2 order-sm-1">
+          <img class="img-info" src="../assets/home/mission.jpg" alt="">
+        </div>
+      </div>
+
+      <div class="row" style="background-color: white; margin: 0;">
+        <div class="img-div col-lg-5 col-md-5 col-sm-12">
+          <img class="img-info" src="../assets/home/join-us.jpg" alt="">
+        </div>
+        <div class="col-lg-7 col-md-7 col-sm-12 p-5">
+          <h2>Volunteer now</h2>
+          <p>Join EcoConnect in our mission for a greener world. Let's turn passion into action and make a difference. 
+            Join us today for a sustainable future.</p>
+          <button type="button" class="btn btn-primary">Volunteer Now!</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -23,8 +65,8 @@
 <script>
 import { mapState, mapMutations } from "vuex"
 import { firebase_firestore, firebase_storage } from "../firebase"
-import { collection, doc, getDocs, deleteDoc } from "firebase/firestore"
-import { deleteObject, ref, getDownloadURL } from "firebase/storage"
+import { collection, getDocs } from "firebase/firestore"
+import { ref, getDownloadURL } from "firebase/storage"
 import OrgNavBar from "../components/OrgNavBar.vue"
 
 export default {
@@ -74,15 +116,7 @@ export default {
 
 <style>
 
-/* content */
 
-.content {
-    position: absolute;
-    width: 100%;
-    padding-left: 3rem;
-    padding-right: 3rem;
-    top: 5rem;
-}
 
 @media (min-width: 992px) {
     .content {
@@ -91,5 +125,27 @@ export default {
         left: 16rem;
         top: 2rem;
     }
+}
+
+.video-wrap {
+    position: relative;
+    overflow: hidden;
+    max-width: 100%;
+}
+
+.custom-video {
+    width: 100%;
+    height: auto;
+    display: block;
+}
+
+.img-info {
+  width: 100%;
+  height: 400px;
+  object-fit: cover;
+}
+
+.img-div {
+  padding: 0;
 }
 </style>
