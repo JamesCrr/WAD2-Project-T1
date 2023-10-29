5f004cba64f3c62a8728d09978ddb0a606980ea9
Joycelyn <joycelynwee.2022@scis.smu.edu.sg>
2023-10-29 18:27:48 +0800

    placeholder cards for organisor/events

--- frontend/src/views/OrganiserEvents.vue
+++ frontend/src/views/OrganiserEvents.vue
@@ -13,7 +13,34 @@
         </button>
       </div>
     </div>
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
+    <div v-if="isLoading">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4 pt-2">
+      <div class="col" v-for="index in placeholderCount" :key="index">
+        <div class="card" aria-hidden="true">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/2560px-Placeholder_view_vector.svg.png"
+            class="card-img-top"
+            style="height: 300px; object-fit: cover"
+          />
+          <div class="card-body d-flex flex-column">
+            <h4 class="card-title placeholder-glow"><span class="placeholder col-6"></span></h4>
+            <span class="placeholder col-7"></span>
+            <span class="placeholder col-4"></span>
+            <span class="placeholder col-4"></span>
+            <span class="placeholder col-6"></span>
+            <span class="placeholder col-8"></span>
+            <div class="grid-container pt-5">
+              <a class="btn btn-primary disabled placeholder col-6" aria-disabled="true" style="width: 100px; "></a>
+              <a class="btn btn-primary disabled placeholder col-6" aria-disabled="true" style="width: 100px; "></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+
+    <div v-else>
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4 pt-2">
       <div class="col" v-for="event in events" :key="event.id">
         <div class="card">
           <img
@@ -43,6 +70,7 @@
         </div>
       </div>
     </div>
+    </div>
   </div>
 </template>
 
@@ -56,11 +84,20 @@ export default {
   data() {
     return {
       events: [],
+      isLoading: true,
+      placeholderCount: 6
     }
   },
   created() {
     // Call the fetchEventData method when the component is created
     this.fetchEventData()
+  },
+  mounted() {
+    setTimeout(() => {
+      // Once data is loaded, set isLoading to false
+      this.isLoading = false;
+      this.fetchEvents() // Fetch events when the component is mounted
+    }, 2000);
   },
   methods: {
     async fetchEventData() {
